871488dfeafa79eb613100f380b66f4199e74e78
FreeFunT0P <ivantarantaev@gmail.com>
2023-06-08 21:09:19 +0300

    add title

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,14 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: '/anime-searcher',
+
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'anime-searcher'
+
+        return args
+      })
+  },
 })
